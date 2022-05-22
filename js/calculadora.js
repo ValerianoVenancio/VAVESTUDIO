a92e0d7a4f0682219d296dde3040a5b48987dd92
ValerianoVenancio <valerianoquessongo@gmail.com>
2022-05-22 16:24:57 +0100

    inicializacao do projecto

---
+++ js/calculadora.js
@@ -0,0 +1,27 @@
+
+
+
+let input1 = document.getElementById("input1");
+let input2 = document.getElementById("input2");
+let elementH2 = document.getElementById("text-resultado");
+
+
+
+function somar(){
+
+   
+
+    let numero1 = parseFloat(input1.value);
+    let numero2 =  parseFloat(input2.value);
+    let resultado = numero1 + numero2; 
+
+
+    elementH2.innerHTML = "O resultado foi: " + resultado;
+
+    input1.value = "";
+    input2.value = "";
+
+}
+
+
+
